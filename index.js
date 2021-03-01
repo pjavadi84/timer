f7c1eb7f3040f7f23d051cf8fc3e268bf86f536d
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-02-28 18:08:14 -0800

    passing callbacks as 4th arg and separated class into Timer file

--- index.js
+++ index.js
@@ -1,49 +1,20 @@
-class Timer {
-    constructor(durationInput, startButton, pauseButton){
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
-
-        this.startButton.addEventListener('click', this.start);
-        this.pauseButton.addEventListener('click', this.pause);
-    }
-
-    start = () => {
-        this.interval = setInterval(this.thick, 1000);
-    }
-
-    pause = () => {
-        clearInterval(this.interval)
-    }
-
-    thick = () => {
-        if(this.timeRemaining <= 0){
-            this.pause()
-        } else {
-            // Hide the complexity of the how the thick works and time remained inside the getter and setter function upon invokation
-            this.timeRemaining -= 1;
-        }
-        
-    }
-
-    // get cause the function to immediately invoked as soon as it is being called
-    get timeRemaining(){
-        // this is how we get the time remaining value
-        return parseFloat(this.durationInput.value);
-    }
-
-    set timeRemaining(time){
-        // this is how to set the time value 
-        this.durationInput.value = time;
-    }
-}
-
-
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput,startButton, pauseButton);
+const timer = new Timer(durationInput,startButton, pauseButton, {
+    onStart(){
+        console.log("Timer started!")
+    },
+
+    onTick(){
+        console.log("Ticking started")
+    },
+
+    onComplete(){
+        console.log("timer complete")
+    }
+});
 
 
 
---
+++ timer.js
@@ -0,0 +1,54 @@
+class Timer {
+    constructor(durationInput, startButton, pauseButton, callbacks){
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+        if(callbacks){
+            this.onStart = callbacks.onStart
+            this.onTick = callbacks.onTick
+            this.onComplete = callbacks.onComplete
+        }
+
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+
+    start = () => {
+        if(this.onStart){
+            this.onStart();
+        }
+        this.tick()
+        this.interval = setInterval(this.tick, 1000);
+    }
+
+    pause = () => {
+        clearInterval(this.interval)
+    }
+
+    tick = () => {
+        if(this.timeRemaining <= 0){
+            this.pause()
+            if(this.onComplete){
+                this.onComplete();
+            }
+        } else {
+            // Hide the complexity of the how the thick works and time remained inside the getter and setter function upon invokation
+            this.timeRemaining -= 1;
+            if(this.onTick){
+                this.onTick();
+            }
+        }
+        
+    }
+
+    // get cause the function to immediately invoked as soon as it is being called
+    get timeRemaining(){
+        // this is how we get the time remaining value
+        return parseFloat(this.durationInput.value);
+    }
+
+    set timeRemaining(time){
+        // this is how to set the time value 
+        this.durationInput.value = time;
+    }
+}
